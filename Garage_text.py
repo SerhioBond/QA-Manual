9279de7212785aec53b3f5fb528f48471c2208db
Serge <SergeF1@i.ua>
2022-10-03 18:12:28 +0300

    Garage

---
+++ Garage_text.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.alert import Alert
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+
+options = Options()
+# options.add_argument("start-maximized"); # open Browser in maximized mode
+# options.add_argument("disable-infobars"); # disabling infobars
+# options.add_argument("--disable-extensions"); # disabling extensions
+# options.add_argument("--disable-gpu"); # applicable to windows os only
+# options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service('D:\\QA\\ChromeDriver\\chromedriver.exe'), options=options)
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+
+assert "Home" in driver.page_source
+
+driver.close()
---
+++ Garage_text_in_element.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+options = Options()
+# options.add_argument("start-maximized"); # open Browser in maximized mode
+# options.add_argument("disable-infobars"); # disabling infobars
+# options.add_argument("--disable-extensions"); # disabling extensions
+# options.add_argument("--disable-gpu"); # applicable to windows os only
+# options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service('D:\\QA\\ChromeDriver\\chromedriver.exe'), options=options)
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+
+elem = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH, "//h1[contains(text(), 'Do more')]"), "Do more"))
+
+if elem:
+  print ("Element found")
+  not_found = True
+else:
+  print ("Element not found")
+  not_found = False
+
+assert not_found
+driver.close()
